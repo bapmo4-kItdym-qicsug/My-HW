2423a28b254207035360b39c550fecd3d815f00c
Nika <nikalyalina239@gmail.com>
2025-01-30 20:52:33 +0500

    secound

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-const verification = function( login, password) { 
-    if ( login === "nika@l" && password === "nnnz") { 
-    console.log("You are logged in"); 
-    }
-    else if ( login ==! "nika@l" && password === "nnnz") { 
-    console.log ("Your login is wrong ");
-    }
-    else if ( login === "nika@l" && password ==! "nnnz") { 
-    console.log ("Your password is wrong");
-    }
-    else {
-    console.log("Both inputs are wrong");
+const choosePresedent = function(citizenship, age, criminalRecord) {
+    if (citizenship === "Kazakhstan" && age >= 35 && criminalRecord == "No") {
+        console.log("You can go for President");
+    } else if (citizenship !== "Kazakhstan") {
+        console.log("You can't go for President because you are not a citizen of Kazakhstan");
+    } else if (age < 35) {
+        console.log("You are still young");
+    } else if (criminalRecord === "Yes") {
+        console.log("You can't go for President due to a criminal record");
+    } else {
+        console.log("You can't go for President");
     }
 }
+
+choosePresedent("Kazakhstan", 25, "No"); 
+choosePresedent("Russia", 40, "No"); 
+choosePresedent("Kazakhstan", 40, "No"); 
+choosePresedent("Kazakhstan", 50, "Yes"); 
+choosePresedent("India", 68, "No"); 
+choosePresedent("Kazakhstan", 35, "No"); 
