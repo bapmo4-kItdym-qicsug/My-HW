622ee798bc030990671559073a865ce8a7cc0088
Nika <nikalyalina239@gmail.com>
2025-01-30 20:35:01 +0500

    dd

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-const count = function () {
-for (let i =0; i<=100 ; i++){//инициализация,условия
-    if (i %2 === 0){
-    console.log(i)
+const putOn = function (degrees){
+    if (degrees < 0){
+        console.log("Stay inside");
+    }
+    else if (degrees < 30){
+        console.log("Wear  coat and a hat");
+    }
+    else if (degrees < 50){
+    console.log("Wear a coat");
+}
+    else{
+        console.log("Wear whatever you want");
     }
 }
-}
 
-count()
+putOn(49);
+putOn(29);
+putOn(100);
+putOn(-1);
