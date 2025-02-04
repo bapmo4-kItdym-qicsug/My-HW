ebc333348fbeddf3fcb8210db8ca50f3333d1522
Nika <nikalyalina239@gmail.com>
2025-02-04 21:43:43 +0500

    secound

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-const array = ["Nika", "Nastya", "Enlik", "Dayana", "Asema", "Liza", "Ardana", "Aitolkyn"];
-for (let i=0; i<= 7; i++){
-console.log(array[i]);
+for(let i =1; i<=100; i++){
+    if(i%3 === 0 && i%5 === 0){
+        console.log("fizz-buzz");
+    }
+    else if (i%3 === 0){
+        console.log("fizz");
+    }
+    else if (i%5 === 0){
+        console.log("buzz");
+    }
+    else{
+        console.log(i);
+    }
 }
