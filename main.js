f71f15d59f899bdaee8a927aa34d9c69c75f0a4d
Nika <nikalyalina239@gmail.com>
2025-02-04 21:35:59 +0500

    first

--- main.js
+++ main.js
@@ -1,20 +1,4 @@
-const choosePresedent = function(citizenship, age, criminalRecord) {
-    if (citizenship === "Kazakhstan" && age >= 35 && criminalRecord == "No") {
-        console.log("You can go for President");
-    } else if (citizenship !== "Kazakhstan") {
-        console.log("You can't go for President because you are not a citizen of Kazakhstan");
-    } else if (age < 35) {
-        console.log("You are still young");
-    } else if (criminalRecord === "Yes") {
-        console.log("You can't go for President due to a criminal record");
-    } else {
-        console.log("You can't go for President");
-    }
+const array = ["Nika", "Nastya", "Enlik", "Dayana", "Asema", "Liza", "Ardana", "Aitolkyn"];
+for (let i=0; i<= 7; i++){
+console.log(array[i]);
 }
-
-choosePresedent("Kazakhstan", 25, "No"); 
-choosePresedent("Russia", 40, "No"); 
-choosePresedent("Kazakhstan", 40, "No"); 
-choosePresedent("Kazakhstan", 50, "Yes"); 
-choosePresedent("India", 68, "No"); 
-choosePresedent("Kazakhstan", 35, "No"); 
