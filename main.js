dd61212811818ce5d0334101fca42d639aa1d75b
Nika <nikalyalina239@gmail.com>
2025-02-06 20:13:12 +0500

    secound

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const array = ["Nika", "Nastya", "Enlik", "Dayana", "Asema", "Liza", "Ardana", "Aitolkyn"];
-for (let i=0; i<= 7; i++){
+for (let i=0; i< array.length-1; i++){
 console.log(array[i]);
 }
