f3d7df50d3cd9354bb66d083418a2b48e57471c9
Nika <nikalyalina239@gmail.com>
2025-02-22 17:28:53 +0500

    first

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
-let array = ["Google", "Apple", "Visa", "Netflex", "Verizon", "Coca-Cola", "Nike" ,"Disney", "Instagram", "Orange"]
-function chooceCompany (){
-    for (let i = 0; i < array.length; i++){
-        if(i === 0){
-            console.log("My choise first " + array [0]);
-        }
-        else if ( i === 1){
-            console.log("My choise secound " + array [1])
-        }
-        else if ( i === 2){
-            console.log("My choise therd " + array [2])
-        }
-        else{
-            console.log("My choise " + array [i])
-        }
-    }
+const findword= function(drow){
+    const word1 = drow;
+    const words = word1.split('');
+    words.reverse();
+    const word2 = words.join('');
+    console.log(word2);
 }
 
-chooceCompany();
+findword("Civic");
+findword("Racecar");
+findword("Testset");
+findword("Madam");
